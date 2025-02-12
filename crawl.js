14a315185974b860f4363c484113e612c9f6d0db
Vishwa5395 <tiwarivishwanath5395@gmail.com>
2025-02-12 23:12:43 +0530

    crawl page recursive approach

--- crawl.js
+++ crawl.js
@@ -1,24 +1,52 @@
 const {JSDOM} = require('jsdom')
 
 
-async function crawlPage(currentURL){
+async function crawlPage(baseURL,currentURL,pages){
+    
+    const baseURLobj=new URL(baseURL)
+    const currentURLobj=new URL(currentURL)
+    if(baseURLobj.hostname!==currentURLobj.hostname){
+        return pages
+    }
+
+    const normalizeCurrentURLs = normalizeURL(currentURL)
+    if(pages[normalizeCurrentURLs]>0){
+        pages[normalizeCurrentURLs]++
+        return pages
+    }
+
+    pages[normalizeCurrentURLs]=1
+
+
+
     console.log(`Actively Crawling: ${currentURL}`)
     try{
         const response=await fetch(currentURL)
 
         if(response.status>399){
             console.log(`error in fetch with status : ${response.status} on the crawling page: ${currentURL}`)
-            return
+            return pages
         }
         const contentType=response.headers.get("Content-type")
         if(!contentType.includes("text/html")){
             console.log(`non html response, content type: ${contentType} on the crawling page: ${currentURL}`)
-            return
+            return pages
         }
-        console.log(await response.text())
+
+        const htmlBody=await response.text()
+        
+        const nextUrl=getURLsfromHTML(htmlBody,baseURL)
+
+        for(const nexturl of nextUrl){
+            pages=await crawlPage(baseURL,nexturl,pages)
+        }
+
+
     }catch(err){
         console.log(`error in fetch: ${err.message}, on current crawling page ${currentURL}`)
     }
+
+    return pages
 }
 
 
@@ -46,7 +74,7 @@ function getURLsfromHTML(htmlBody,baseURL){
             }catch(err){
                 console.log(`Error parsing relative URL: ${err.message}`)
             }
-        }
+        }   
     }
     return urls
 }
