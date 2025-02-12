e96dbf8be2ea6fe457583d0af9e0e313fdf17e81
Vishwa5395 <tiwarivishwanath5395@gmail.com>
2025-02-12 19:28:45 +0530

    Normalize URLs and tests

---
+++ crawl.test.js
@@ -0,0 +1,54 @@
+const{normalizeURL}=require('./crawl.js')
+const{test,expect}=require('@jest/globals')
+
+
+test('normalizeURL strip protocol',()=>{
+    const input='https://boot.dev/path'
+    const actual=normalizeURL(input)
+    const expected='boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('normalizeURL strip trailing slash',()=>{
+    const input='https://boot.dev/path/'
+    const actual=normalizeURL(input)
+    const expected='boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('normalizeURL capitals',()=>{
+    const input='https://BOOT.dev/path/'
+    const actual=normalizeURL(input)
+    const expected='boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+
+test('normalizeURL strip http',()=>{
+    const input='https://boot.dev/path/'
+    const actual=normalizeURL(input)
+    const expected='boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
